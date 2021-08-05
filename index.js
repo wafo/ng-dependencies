58be90adbf15095750721f685d35c1eed953d795
Jose Luis Vega <jvega@paystand.com>
2021-08-05 09:21:33 -0500

    Trying babel parser

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
-var esprima = require('esprima');
-var estraverse = require('estraverse');
-var _ = require('lodash');
+var esprima = require("esprima");
+var estraverse = require("estraverse");
+var _ = require("lodash");
+var babelParser = require("@babel/parser");
 
 function parseOptions(opts) {
   var defaultOpts = {
     // default options
   };
-  return _.merge(defaultOpts, (opts || {}));
+  return _.merge(defaultOpts, opts || {});
 }
 
 function findDependencies(source, opts) {
@@ -18,29 +19,38 @@ function findDependencies(source, opts) {
   var rootDeps = [];
   var modules = {};
 
-  estraverse.traverse(esprima.parse(source, {sourceType: "module"}), {
-    leave: function(node, parent) {
-      if (canBeModuleNameVariableDeclaration(node)) {
-        potentialModuleNameVariable[node.id.name] = node.init.value;
-      }
-
-      if (!isAngularModuleStatement(node)) {
-        if (isNgModuleDeclaration(node)) {
-          modules['ng'] = [];
+  // estraverse.traverse(esprima.parse(source, { sourceType: "module" }), {
+  estraverse.traverse(
+    babelParser.parse(source, {
+      sourceType: "unambiguous",
+      plugins: ["jsx"],
+    }),
+    {
+      leave: function (node, parent) {
+        if (canBeModuleNameVariableDeclaration(node)) {
+          potentialModuleNameVariable[node.id.name] = node.init.value;
         }
-        return;
-      }
 
-      var moduleNameArg = parent.arguments[0];
-      var moduleName = moduleNameArg.value || potentialModuleNameVariable[moduleNameArg.name];
-      if (parent.arguments[1]) {
-        // if already declared, will reset dependencies, like how angular behaves (latest declaration wins)
-        modules[moduleName] = _.map(parent.arguments[1].elements, 'value');
-      } else {
-        rootDeps.push(moduleName);
-      }
+        if (!isAngularModuleStatement(node)) {
+          if (isNgModuleDeclaration(node)) {
+            modules["ng"] = [];
+          }
+          return;
+        }
+
+        var moduleNameArg = parent.arguments[0];
+        var moduleName =
+          moduleNameArg.value ||
+          potentialModuleNameVariable[moduleNameArg.name];
+        if (parent.arguments[1]) {
+          // if already declared, will reset dependencies, like how angular behaves (latest declaration wins)
+          modules[moduleName] = _.map(parent.arguments[1].elements, "value");
+        } else {
+          rootDeps.push(moduleName);
+        }
+      },
     }
-  });
+  );
 
   var moduleKeys = _.keys(modules);
   var moduleValues = _.values(modules);
@@ -50,28 +60,41 @@ function findDependencies(source, opts) {
   rootDeps = _.difference(rootDeps, moduleKeys);
 
   var isAngular = moduleKeys.length > 0 || rootDeps.length > 0;
-  if (isAngular && !_.has(modules, 'ng') && !_.some(rootDeps, 'ng')) {
-    rootDeps.unshift('ng');
+  if (isAngular && !_.has(modules, "ng") && !_.some(rootDeps, "ng")) {
+    rootDeps.unshift("ng");
   }
 
   var ret = {
     dependencies: rootDeps,
-    modules: modules
+    modules: modules,
   };
 
   return ret;
 }
 
 function isAngularModuleStatement(node) {
-  return node.type === 'MemberExpression' && node.object.name === 'angular' && node.property.name === 'module';
+  return (
+    node.type === "MemberExpression" &&
+    node.object.name === "angular" &&
+    node.property.name === "module"
+  );
 }
 
 function isNgModuleDeclaration(node) {
-  return node.type === 'CallExpression' && node.callee.name === 'angularModule' && node.arguments.length > 0 && node.arguments[0].value === 'ng';
+  return (
+    node.type === "CallExpression" &&
+    node.callee.name === "angularModule" &&
+    node.arguments.length > 0 &&
+    node.arguments[0].value === "ng"
+  );
 }
 
 function canBeModuleNameVariableDeclaration(node) {
-  return node.type === 'VariableDeclarator' && node.init && typeof node.init.value === 'string';
+  return (
+    node.type === "VariableDeclarator" &&
+    node.init &&
+    typeof node.init.value === "string"
+  );
 }
 
 module.exports = findDependencies;
